3c6a410a9b026b9aa8a7b2827c05d8cad1b7d7d8
Lemasc <lemascserviceth@hotmail.com>
2021-08-27 13:43:54 +0700

    Webpack

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: function (config, { dev, isServer }) {
+    if (!isServer) {
+        config.resolve.fallback.fs = false
+    }
+    if (!dev) {
+        config.plugins.push(
+            new CopyPlugin({
+                patterns: [{ from: "candidates", to: "candidates" }],
+            })
+        )
+    }
+    return config
+},
 }
