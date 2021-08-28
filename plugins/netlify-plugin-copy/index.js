64f25bd793c21848ddb9cc128458e23ed649fc42
Lemasc <lemascserviceth@hotmail.com>
2021-08-28 09:01:48 +0700

    Copy plugin final?

--- plugins/netlify-plugin-copy/index.js
+++ plugins/netlify-plugin-copy/index.js
@@ -5,12 +5,12 @@ const fs = require("fs-extra")
  */
 module.exports = {
     onBuild: async () => {
-      console.log("Copying assets to Netlify functions..")
+      console.log("Copying candidates data to Netlify functions..")
       const dir = (await fs.readdir("./serverless")).filter(d => d.includes("select"))
       await Promise.all(dir.map(async(d) => {
           // Copy into this directory
           console.log(`- ${d}`)
-          console.log(await fs.copy(`./candidates`,`./serverless/${d}`))
+          await fs.copy(`./candidates`,`./serverless/${d}/candidates`)
           console.log(await fs.readdir(`./serverless/${d}`))
       }))
       console.log("Copied successfully.")
