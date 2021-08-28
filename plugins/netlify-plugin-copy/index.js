7620fc561f109eeebd109c5b755a92e20017b012
Lemasc <lemascserviceth@hotmail.com>
2021-08-28 08:45:29 +0700

    Dir

--- plugins/netlify-plugin-copy/index.js
+++ plugins/netlify-plugin-copy/index.js
@@ -4,9 +4,10 @@ const fs = require("fs-extra")
  * This is not require for getStaticProps, since it can read from the filesystem out-of-the-box.
  */
 module.exports = {
-    onPostBuild: async () => {
+    onBuild: async () => {
       console.log(await fs.readdir("./"))
       const dir = await fs.readdir("./serverless")
+      console.log(dir)
       await Promise.all(dir.map(async(d) => {
         if(d.includes("select")) {
           // Copy into this directory
