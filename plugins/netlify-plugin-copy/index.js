ecd04ed1517f83263349d3c9537dc3232208b8ce
Lemasc <lemascserviceth@hotmail.com>
2021-08-28 08:41:46 +0700

    Plugin2

--- plugins/netlify-plugin-copy/index.js
+++ plugins/netlify-plugin-copy/index.js
@@ -1,9 +1,17 @@
-const fs = require("fs")
-const {promises} = fs
+const fs = require("fs-extra")
+/**
+ * For netlify, we need to copy the candidates data to the functions individually.
+ * This is not require for getStaticProps, since it can read from the filesystem out-of-the-box.
+ */
 module.exports = {
     onPostBuild: async () => {
-      console.log(await promises.readdir("./"))
-      console.log(await promises.readdir("../"))
-      console.log(await promises.readdir("../../"))
+      console.log(await fs.readdir("./"))
+      const dir = await fs.readdir("./serverless")
+      await Promise.all(dir.map(async(d) => {
+        if(d.includes("select")) {
+          // Copy into this directory
+          console.log(d,await fs.readdir(d))
+        }
+      }))
     },
   }
