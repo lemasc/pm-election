8ed23c450e394fbc9fda55347a6caf0f1ad88cfd
Lemasc <lemascserviceth@hotmail.com>
2021-08-29 23:15:07 +0700

    Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,15 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
-const db = admin.firestore();
+const auth = admin.auth();
+const db = admin.firestore;
 
 // Updates the summary when votes are added, edited, or delete.
-// The summary will then used by dashboard
-exports.updateSummary = functions.firestore.document("/votes/{votesId}")
-    .onWrite((change, context) => {
-      if (context.params.votesId === "summary") return;
+// The summary will then used by dashboard.
+exports.updateSummary = functions.firestore.document("/votes/{userId}")
+    .onWrite(async (change, context) => {
+      const uid = context.params.userId;
+      if (uid === "summary") return;
       // Get an object with the current document value.
       // If the document does not exist, it has been deleted.
       const document = change.after.exists ? change.after.data() : undefined;
@@ -18,24 +20,36 @@ exports.updateSummary = functions.firestore.document("/votes/{votesId}")
       let fields = {};
       if (oldDocument && document &&
         oldDocument.selected === document.selected) return;
+
+      const user = await auth.getUser(uid);
       if (oldDocument) {
         fields = {
           ...fields,
           [oldDocument.selected]: oldDocument[oldDocument.selected] &&
           oldDocument[oldDocument.selected] > 0 ?
-          admin.firestore.FieldValue.increment(-1) : 0,
+          db.FieldValue.increment(-1) : 0,
         };
+        if (!document) {
+          await auth.setCustomUserClaims(uid, {
+            ...user.customClaims,
+            voted: undefined,
+          });
+        }
       }
       functions.logger.log("Document", document);
       if (document) {
         fields = {
           ...fields,
-          [document.selected]: admin.firestore.FieldValue.increment(1),
+          [document.selected]: db.FieldValue.increment(1),
         };
+        await auth.setCustomUserClaims(uid, {
+          ...user.customClaims,
+          voted: document.selected,
+        });
       }
 
       functions.logger.log("Updating summary", context.params.votesId, fields);
-      db.collection("votes").doc("summary").set({
+      return await db().collection("votes").doc("summary").set({
         ...fields,
         timestamp: admin.firestore.FieldValue.serverTimestamp(),
       }, {
@@ -43,3 +57,22 @@ exports.updateSummary = functions.firestore.document("/votes/{votesId}")
       });
     });
 
+// Add the users to the summary.
+exports.addUsers = functions.auth.user().onCreate(() => {
+  db().collection("votes").doc("summary").set({
+    users: admin.firestore.FieldValue.increment(1),
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+  }, {
+    merge: true,
+  });
+});
+
+// Remove the users from the summary.
+exports.removeUsers = functions.auth.user().onDelete(() => {
+  db().collection("votes").doc("summary").set({
+    users: admin.firestore.FieldValue.increment(-1),
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+  }, {
+    merge: true,
+  });
+});
