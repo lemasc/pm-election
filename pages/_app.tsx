7cbea441828f12048811d8520f3b677e3964ee22
Lemasc <lemascserviceth@hotmail.com>
2021-09-03 12:36:55 +0700

    Logrocket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "../styles/content.css";
 import type { AppProps } from "next/app";
+import LogRocket from "logrocket";
 import MainProvider from "@/shared/provider";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  LogRocket.init("y60w1s/election");
   return (
     <MainProvider>
       <Component {...pageProps}></Component>
